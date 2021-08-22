f4a535894cedddd0f4ed75a2e2ca3a7539c0852e
Felipe Farias <fefarias97@gmail.com>
2021-08-22 20:37:17 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Santos");
+            Console.WriteLine("Guaruja");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruibe");
+        }
+    }
+}
